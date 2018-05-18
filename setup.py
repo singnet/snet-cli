94e6ec8e6bb8ddf18c60caee7dd6f1a7bdabf1ef
Eric Kearns <erckrns@gmail.com>
2018-05-18 12:49:25 -0700

    Update readme with pycryptodome instructions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snet-cli',
-    version='0.1.0',
+    version='0.1.1',
     packages=['snet_cli'],
     scripts=['bin/snet'],
     url='https://github.com/singnet/snet-cli',
