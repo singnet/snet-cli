b4c723ec0dfb305a014a58c1d8cb97ce0ae1180f
arturgontijo <arturgontijo@gmail.com>
2018-09-18 14:13:10 -0300

    [organization] Synced

--- snet_cli/commands.py
+++ snet_cli/commands.py
@@ -98,6 +98,10 @@ class Command(object):
         SessionCommand(config or self.config, DefaultAttributeObject(key=key, value=value), out_f or self.out_f,
                        err_f or self.err_f).set()
 
+    def _unset_key(self, key, config=None, out_f=None, err_f=None):
+        SessionCommand(config or self.config, DefaultAttributeObject(key=key), out_f or self.out_f,
+                       err_f or self.err_f).unset()
+
     def _nothing(self):
         pass
 
@@ -779,6 +783,37 @@ class ServiceCommand(BlockchainCommand):
             self.w3 = get_web3(network)
         return self.w3.version.network
 
+    def _getserviceregistrationbyname(self, service_json=None):
+        try:
+            if service_json:
+                organization = service_json["organization"]
+                service_name = service_json["name"]
+            elif self.args.organization and self.args.name:
+                organization = self.args.organization
+                service_name = self.args.name
+            else:
+                self._error("Fail to get ORG_NAME and SERVICE_NAME...")
+
+            registry_contract_def = get_contract_def("Registry")
+            registry_address = self._getstring("registry_at")
+
+            return ContractCommand(
+                config=self.config,
+                args=self.get_contract_argser(
+                    contract_address=registry_address,
+                    contract_function="getServiceRegistrationByName",
+                    contract_def=registry_contract_def)(type_converter("bytes32")(organization),
+                                                        type_converter("bytes32")(service_name)),
+                out_f=None,
+                err_f=None,
+                w3=self.w3,
+                ident=self.ident).call()
+
+        except Exception as e:
+            self._printerr("\nCall getServiceRegistrationByName() error!\nHINT: Check your identity and session.\n")
+            self._error(e)
+
+
     def init(self):
         accept_all_defaults = self.args.y
         init_args = {
@@ -850,8 +885,11 @@ class ServiceCommand(BlockchainCommand):
         else:
             service_json_path = "service.json"
 
-        with open(service_json_path) as f:
-            service_json = json.load(f)
+        try:
+            with open(service_json_path) as f:
+                service_json = json.load(f)
+        except Exception as e:
+            self._error("Failed to load {}!".format(service_json_path))
 
         agent_address = service_json.get('networks', {}).get(network_id, {}).get('agentAddress', None)
         need_agent = agent_address is None
@@ -961,17 +999,7 @@ class ServiceCommand(BlockchainCommand):
             registry_contract_def = get_contract_def("Registry")
             registry_address = self._getstring("registry_at")
 
-            (found, _, current_path, current_agent_address, current_tags) = ContractCommand(
-                config=self.config,
-                args=self.get_contract_argser(
-                    contract_address=registry_address,
-                    contract_function="getServiceRegistrationByName",
-                    contract_def=registry_contract_def)(type_converter("bytes32")(organization),
-                                                        type_converter("bytes32")(service_json["name"])),
-                out_f=None,
-                err_f=None,
-                w3=self.w3,
-                ident=self.ident).call()
+            (found, _, current_path, current_agent_address, current_tags) = self._getserviceregistrationbyname(service_json)
 
             if found:
                 if (current_path != type_converter("bytes32")(service_json["path"]) or
@@ -1082,17 +1110,7 @@ class ServiceCommand(BlockchainCommand):
         registry_contract_def = get_contract_def("Registry")
         registry_address = self._getstring("registry_at")
 
-        (found, _, current_path, current_agent_address, current_tags) = ContractCommand(
-            config=self.config,
-            args=self.get_contract_argser(
-                contract_address=registry_address,
-                contract_function="getServiceRegistrationByName",
-                contract_def=registry_contract_def)(type_converter("bytes32")(service_json["organization"]),
-                                                    type_converter("bytes32")(service_json["name"])),
-            out_f=None,
-            err_f=None,
-            w3=self.w3,
-            ident=self.ident).call()
+        (found, _, current_path, current_agent_address, current_tags) = self._getserviceregistrationbyname(service_json)
 
         if not found:
             self._error("Service '{}' not registered on network with id {}".format(service_json["name"], network_id))
@@ -1288,6 +1306,46 @@ class ServiceCommand(BlockchainCommand):
         self._set_key("current_agent_at", agent_address, out_f=self.err_f)
         self._printout("Service is updated!")
         return
+
+    def delete(self):
+
+        try:
+            self._printout("Getting information about the service...")
+            network_id = self._get_network()
+
+            (found, _, current_path, current_agent_address, current_tags) = self._getserviceregistrationbyname()
+
+            if not found:
+                self._error("Service {} is not registered on network with id {}".format(self.args.name, network_id))
+            else:
+                self._printerr("Deleting service {}...".format(self.args.name))
+                registry_contract_def = get_contract_def("Registry")
+                registry_address = self._getstring("registry_at")
+                cmd = ContractCommand(
+                    config=self.config,
+                    args=self.get_contract_argser(
+                        contract_address=registry_address,
+                        contract_function="deleteServiceRegistration",
+                        contract_def=registry_contract_def)(type_converter("bytes32")(self.args.organization),
+                                                            type_converter("bytes32")(self.args.name)),
+                    out_f=None,
+                    err_f=None,
+                    w3=self.w3,
+                    ident=self.ident)
+                try:
+                    cmd.transact()
+                except Exception as e:
+                    self._printerr("\nTransaction error!\nHINT: Check your session and service json file.\n")
+                    self._error(e)
+
+            # Updating session
+            self._printerr("Removing current contract address from session...\n")
+            self._unset_key("current_agent_at", out_f=self.err_f)
+            self._printout("Service was deleted!")
+
+        except Exception as e:
+            self._printerr("\nTransaction error!\nHINT: Check ORG_NAME and SERVICE_NAME..\n")
+            self._error(e)
 
 
 class OrganizationCommand(BlockchainCommand):
