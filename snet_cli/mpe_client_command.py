7bbf8f34eb48331c498942a8eab70923fb96ab14
Sergey Rodionov <astroseger@gmail.com>
2018-10-12 13:54:38 +0300

    add safe_to_checksum_address

--- snet_cli/mpe_client_command.py
+++ snet_cli/mpe_client_command.py
@@ -5,14 +5,20 @@ import base64
 class MPEClientCommand(BlockchainCommand):
     def sign_message(self):
         # here it is ok to accept address without checksum
-        mpe_address = self.w3.toChecksumAddress(self.args.mpe_address)
+        mpe_address = self.safe_to_checksum_address(self.args.mpe_address, error_message = "Wrong format of MultiPartyEscrow address")
         
         message = self.w3.soliditySha3(
         ["address",   "uint256",            "uint256",       "uint256"],
         [mpe_address, self.args.channel_id, self.args.nonce, self.args.amount])
         
         sign = self.ident.sign_message_after_soliditySha3(message)
-        return sign
+        return sign    
+    
+    def safe_to_checksum_address(self, a, error_message):
+        try:
+            return self.w3.toChecksumAddress(a)
+        except ValueError as err:
+            self._error("%s\n%s"%(error_message,err))
     
     def print_sign_message(self):
         sign = self.sign_message()
