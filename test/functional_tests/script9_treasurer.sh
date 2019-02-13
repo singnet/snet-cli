0169feee7b785535d2cae9139be63d22a5d0ffd9
Sergey Rodionov <astroseger@gmail.com>
2019-02-13 23:42:53 +0300

    add tests for 'snet client call-lowlevel' and 'snet client  get-channel-state'. Also add tests for multiply endpoints in metadata (keeping tests for sinlge endpoint in metadata as well)

--- test/functional_tests/script9_treasurer.sh
+++ test/functional_tests/script9_treasurer.sh
@@ -10,6 +10,7 @@ cd ..
 snet account deposit 12345 -y -q
 
 # service provider has --wallet-index==9 (0x52653A9091b5d5021bed06c5118D24b23620c529)
+# make two endpoints (both are actually valid)
 snet service metadata-init ./service_spec1/ ExampleService 0x52653A9091b5d5021bed06c5118D24b23620c529 --fixed-price 0.0001 --endpoints 127.0.0.1:50051
 
 
@@ -22,12 +23,22 @@ EXPIRATION0=$((`snet channel block-number` + 100))
 EXPIRATION1=$((`snet channel block-number` + 100000))
 EXPIRATION2=$((`snet channel block-number` + 100000))
 snet channel open-init-metadata testo tests0 1 $EXPIRATION0 -yq
+
+# add second endpoint to metadata (in order to test case with two endpoints in metadata)
+snet service metadata-add-endpoints localhost:50051
+
+# formally we will have two open chanels (channnel_id = 1,2) for the same service (testo/tests1)
 snet channel open-init-metadata testo tests1 1 $EXPIRATION1 -yq --open-new-anyway
 snet channel open-init-metadata testo tests1 1 $EXPIRATION2 -yq --open-new-anyway
 
-snet client call testo tests0 classify {}
+#low level equivalent to "snet client call testo tests0 classify {}"
+snet client call-lowlevel testo tests0 0 0 10000 classify {}
+# should fail because nonce is incorect
+snet client call-lowlevel testo tests0 0 1 20000 classify {} && exit 1 || echo "fail as expected"
+# should fail because amount is incorect
+snet client call-lowlevel testo tests0 0 0 10000 classify {} && exit 1 || echo "fail as expected"
 snet client call testo tests1 classify {} --save-response response.pb  --channel-id 1 --endpoint http://127.0.0.1:50051
-snet client call testo tests1 classify {} --save-field binary_field out.bin  --channel-id 2
+snet client call testo tests1 classify {} --save-field binary_field out.bin  --channel-id 2 --endpoint http://localhost:50051
 snet client call testo tests1 classify {} --save-field predictions out.txt --channel-id 2
 rm -f response.pb out.bin out.txt
 snet treasurer claim-all --endpoint 127.0.0.1:50051  --wallet-index 9 -yq
@@ -35,7 +46,12 @@ snet treasurer claim-all --endpoint 127.0.0.1:50051  --wallet-index 9 -yq
 assert_balance 0.0004
 snet client call testo tests0 classify {}
 snet client call testo tests0 classify {}
-snet client call testo tests1 classify {} --channel-id 1
+snet client  get-channel-state 0 http://localhost:50051
+snet client  get-channel-state 1 http://127.0.0.1:50051
+
+# low level equivalent to "snet client call testo tests1 classify {} --channel-id 1"
+snet client call-lowlevel testo tests1 1 1 10000 classify {}
+
 snet client call testo tests1 classify {} --channel-id 2
 
 #only channel 0 should be claimed
