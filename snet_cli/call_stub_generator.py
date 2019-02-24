4e53640b175ef3e34762f811c15464bfc50ac9c9
Sergey Rodionov <astroseger@gmail.com>
2019-02-25 00:21:00 +0300

    implement __call__ in object returned by call_stub_generator

--- snet_cli/call_stub_generator.py
+++ snet_cli/call_stub_generator.py
@@ -5,4 +5,9 @@ from snet_cli.utils import DefaultAttributeObject
 def call_stub_generator(org_id, service_id, method, **kwargs):
     conf = Config()
     args = DefaultAttributeObject(org_id = org_id, service_id = service_id, method = method, yes = True, **kwargs)
-    return MPEClientCommand(conf, args)
+    class Stub:
+        def __call__(self, params):
+            return self.client.call_server_statelessly_with_params(params)
+    stub = Stub()
+    stub.client = MPEClientCommand(conf, args)
+    return stub
