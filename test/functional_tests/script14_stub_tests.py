edb8024eb4e549c330f0f7af221164ec510e82fe
Sergey Rodionov <astroseger@gmail.com>
2019-02-25 00:22:17 +0300

    fix tests for new stub generator

--- test/functional_tests/script14_stub_tests.py
+++ test/functional_tests/script14_stub_tests.py
@@ -2,5 +2,5 @@ from snet_cli.call_stub_generator import call_stub_generator
 
 stub = call_stub_generator("testo", "tests", "classify")
 
-rez = stub.call_server_statelessly_with_params({})
+rez = stub({})
 print(rez)
