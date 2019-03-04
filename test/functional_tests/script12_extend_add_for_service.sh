f6062ca5bf19d8b3918dc18344015f51563d3371
Sergey Rodionov <astroseger@gmail.com>
2019-03-04 19:00:44 +0300

    add tests for 'snet channel extend-add-for-service' in case of separate use of '--amount' and '--expiration'

--- test/functional_tests/script12_extend_add_for_service.sh
+++ test/functional_tests/script12_extend_add_for_service.sh
@@ -11,28 +11,28 @@ snet channel open-init testo tests 123.123 1 -yq
 snet channel print-initialized | grep 123.223 && exit 1 || echo "fail as expected"
 
 snet channel extend-add-for-service testo tests --amount 0.1 --expiration 314 -yq
-snet channel add-for-service testo tests 0.2 -yq
-snet channel extend-for-service testo tests 315 -yq
+snet channel extend-add-for-service testo tests --amount 0.2 -yq
+snet channel extend-add-for-service testo tests --expiration 315 -yq
 snet channel print-initialized | grep 123.423
 snet channel print-initialized | grep 315
 
 
 rm -rf ~/.snet/mpe_client
 snet channel extend-add-for-service testo tests --amount 0.1 --expiration 315 -yq
-snet channel add-for-service testo tests 0.1  -yq
-snet channel extend-for-service testo tests 31415  -yq
+snet channel extend-add-for-service testo tests --amount 0.1  -yq
+snet channel extend-add-for-service testo tests --expiration 31415  -yq
 snet channel print-initialized | grep 123.623
 snet channel print-initialized | grep 31415
 
 
 snet channel open-init testo tests 7777.8888 1 -yq --open-new-anyway
 snet channel extend-add-for-service testo tests --amount 0.01 --expiration 314 -yq && exit 1 || echo "fail as expected"
-snet channel add-for-service        testo tests 0.01 -yq && exit 1 || echo "fail as expected"
-snet channel extend-for-service     testo tests 31477 -yq && exit 1 || echo "fail as expected"
+snet channel extend-add-for-service testo tests --amount 0.01 -yq && exit 1 || echo "fail as expected"
+snet channel extend-add-for-service testo tests --expiration 31477 -yq && exit 1 || echo "fail as expected"
 
 snet channel extend-add-for-service testo tests --amount 0.01 --expiration 314 -yq --channel-id 1
-snet channel add-for-service        testo tests 0.01  --channel-id 1 -yq
-snet channel extend-for-service     testo tests 31477 -yq --channel-id 1
+snet channel extend-add-for-service testo tests --amount 0.01  --channel-id 1 -yq
+snet channel extend-add-for-service testo tests --expiration 31477 -yq --channel-id 1
 snet channel print-initialized | grep 7777.9088
 snet channel print-initialized | grep 31477
 
@@ -46,13 +46,13 @@ snet service publish testo tests2 -y -q
 snet channel open-init testo tests2 2222.33333 1 -yq --group-name group1
 
 snet channel extend-add-for-service testo tests2 --amount 0.001 --expiration 314 -yq && exit 1 || echo "fail as expected"
-snet channel add-for-service        testo tests2  0.001 -yq && exit 1 || echo "fail as expected"
-snet channel extend-for-service     testo tests2 4321 -yq && exit 1 || echo "fail as expected"
+snet channel extend-add-for-service testo tests2 --amount 0.001 -yq && exit 1 || echo "fail as expected"
+snet channel extend-add-for-service testo tests2 --expiration 4321 -yq && exit 1 || echo "fail as expected"
 
 
 snet channel extend-add-for-service testo tests2 --amount 0.001 --expiration 4321 --group-name group1 -yq
-snet channel add-for-service        testo tests2  0.001  --group-name group1 -yq
-snet channel extend-for-service     testo tests2 4321 --group-name group1 -yq
+snet channel extend-add-for-service testo tests2 --amount 0.001  --group-name group1 -yq
+snet channel extend-add-for-service testo tests2 --expiration 4321 --group-name group1 -yq
 
 snet channel print-initialized | grep 2222.33533
 snet channel print-initialized | grep 4321
@@ -60,8 +60,8 @@ snet channel print-initialized | grep 4321
 
 snet channel open-init testo tests2 2222.33333 1 -yq --group-name group2
 snet channel extend-add-for-service testo tests2 --amount 0.0001 --expiration 314 -yq --group-name group2
-snet channel add-for-service        testo tests2  0.0001  --group-name group2 -yq
-snet channel extend-for-service     testo tests2 5643 --group-name group2 -yq
+snet channel extend-add-for-service testo tests2 --amount 0.0001  --group-name group2 -yq
+snet channel extend-add-for-service testo tests2 --expiration 5643 --group-name group2 -yq
 snet channel print-initialized | grep 2222.33353
 snet channel print-initialized | grep 5643
 
@@ -69,7 +69,7 @@ snet channel print-initialized | grep 5643
 rm -rf ~/.snet/mpe_client
 
 snet channel extend-add-for-service testo tests2 --amount 0.00001 --expiration 7654 -yq --group-name group2
-snet channel add-for-service        testo tests2  0.00001  --group-name group2 -yq
-snet channel extend-for-service     testo tests2 7655 --group-name group2 -yq
+snet channel extend-add-for-service testo tests2 --amount 0.00001  --group-name group2 -yq
+snet channel extend-add-for-service testo tests2 --expiration 7655 --group-name group2 -yq
 snet channel print-initialized | grep 2222.33355
 snet channel print-initialized | grep 7655
