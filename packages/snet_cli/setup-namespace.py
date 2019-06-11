22ed5db6038c08c21ccebb3ebe4854a63deff15c
Valerio Versace <mail@valerioversace.it>
2019-06-11 05:01:05 +0200

    fixed basic setup for CLI, added setup and requirements for namespace package, moved version identifier

--- packages/snet_cli/setup-namespace.py
+++ packages/snet_cli/setup-namespace.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages, find_namespace_packages
+from setuptools import setup, find_namespace_packages
 from setuptools.command.develop import develop as _develop
 from setuptools.command.install import install as _install
 
@@ -35,15 +35,15 @@ version_dict = {}
 with open("./snet/snet_cli/version.py") as fp:
     exec(fp.read(), version_dict)
 setup(
-    name='snet-cli',
+    name='snet.snet-cli',
     version=version_dict['__version__'],
-    packages=find_namespace_packages(include=['snet.*'])+find_packages(),
+    packages=find_namespace_packages(include=['snet.*']),
     namespace_packages=['snet'],
     url='https://github.com/singnet/snet-cli',
     license='MIT',
     author='SingularityNET Foundation',
     author_email='info@singularitynet.io',
-    description='SingularityNET CLI',
+    description='SingularityNET CLI standalone namespace package',
     python_requires='>=3.6',
     install_requires=[
         'grpcio-tools==1.19.0',
@@ -51,19 +51,10 @@ setup(
         'web3==4.8.3',
         'mnemonic==0.18',
         'pycoin>=0.80',
-        'trezor==0.9.1',
         'rlp==1.0.1',
         'pyyaml>=4.2b1',
         'ipfsapi==0.4.2.post1',
         'rfc3986==1.1.0',
-        'hidapi>=0.7.99',  # _vendor/ledgerblue
-        'protobuf>=2.6.1',  # _vendor/ledgerblue
-        'pycryptodome>=3.6.6',  # _vendor/ledgerblue
-        'eth-hash>=0.2.0',  # the latest eth-hash v0.2.0 requires pycryptodome>=3.6.6,<4
-        'future==0.16.0',  # _vendor/ledgerblue
-        'ecpy>=0.8.1',  # _vendor/ledgerblue
-        'pillow>=3.4.0',  # _vendor/ledgerblue
-        'python-u2flib-host>=3.0.2',  # _vendor/ledgerblue
         'pymultihash==0.8.2',
         'base58==1.0.2',
         'argcomplete>=1.9.4',
@@ -71,12 +62,7 @@ setup(
     ],
     cmdclass={
         'develop': develop,
-        'install': install,
+        'install': install
     },
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'snet = snet_cli:main',
-        ],
-    }
+    include_package_data=True
 )
