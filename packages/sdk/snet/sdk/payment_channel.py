6160797ab80cc63e9fe9e679740eb72fc86e51d7
Restyled.io <commits@restyled.io>
2019-10-14 08:21:32 +0000

    Restyled by autopep8

--- packages/sdk/snet/sdk/payment_channel.py
+++ packages/sdk/snet/sdk/payment_channel.py
@@ -4,6 +4,7 @@ from eth_account.messages import defunct_hash_message
 
 from snet.snet_cli.utils import RESOURCES_PATH, add_to_path
 
+
 class PaymentChannel:
     def __init__(self, channel_id, w3, account, service, mpe_contract):
         self.channel_id = channel_id
@@ -16,21 +17,18 @@ class PaymentChannel:
             "last_signed_amount": 0
         }
 
-
     def add_funds(self, amount):
         return self.mpe_contract.channel_add_funds(self.account, self.channel_id, amount)
-
 
     def extend_expiration(self, expiration):
         return self.mpe_contract.channel_extend(self.account, self.channel_id, expiration)
 
-
     def extend_and_add_funds(self, expiration, amount):
         return self.mpe_contract.channel_extend_and_add_funds(self.account, self.channel_id, expiration, amount)
 
-
     def sync_state(self):
-        channel_blockchain_data = self.mpe_contract.contract.functions.channels(self.channel_id).call()
+        channel_blockchain_data = self.mpe_contract.contract.functions.channels(
+            self.channel_id).call()
         (current_nonce, last_signed_amount) = self._get_current_channel_state()
         nonce = channel_blockchain_data[0]
         total_amount = channel_blockchain_data[5]
@@ -45,14 +43,16 @@ class PaymentChannel:
             "available_amount": available_amount
         }
 
-
     def _get_current_channel_state(self):
         stub = self.payment_channel_state_service_client
         current_block_number = self.web3.eth.getBlock("latest").number
-        message = web3.Web3.soliditySha3(["string","uint256","uint256"], ["__get_channel_state",self.channel_id,current_block_number])
-        signature = self.web3.eth.account.signHash(defunct_hash_message(message), self.account.signer_private_key).signature
+        message = web3.Web3.soliditySha3(["string", "uint256", "uint256"], [
+                                         "__get_channel_state", self.channel_id, current_block_number])
+        signature = self.web3.eth.account.signHash(defunct_hash_message(
+            message), self.account.signer_private_key).signature
         with add_to_path(str(RESOURCES_PATH.joinpath("proto"))):
             state_service_pb2 = importlib.import_module("state_service_pb2")
-        request = state_service_pb2.ChannelStateRequest(channel_id=web3.Web3.toBytes(self.channel_id), signature=bytes(signature))
+        request = state_service_pb2.ChannelStateRequest(
+            channel_id=web3.Web3.toBytes(self.channel_id), signature=bytes(signature))
         response = stub.GetChannelState(request)
         return int.from_bytes(response.current_nonce, byteorder="big"), int.from_bytes(response.current_signed_amount, byteorder="big")
--- packages/sdk/snet/sdk/service_client.py
+++ packages/sdk/snet/sdk/service_client.py
@@ -19,7 +19,7 @@ class _ClientCallDetails(
 
 
 class ServiceClient:
-    def __init__(self, sdk, service_metadata,group, service_stub, payment_channel_management_strategy,
+    def __init__(self, sdk, service_metadata, group, service_stub, payment_channel_management_strategy,
                  options):
         self.sdk = sdk
         self.options = options
@@ -36,27 +36,27 @@ class ServiceClient:
         self.payment_channels = []
         self.last_read_block = 0
 
-
     def _get_payment_expiration_threshold_for_group(self):
         pass
 
-
     def _generate_grpc_stub(self, service_stub):
         grpc_channel = self._base_grpc_channel
-        disable_blockchain_operations = self.options.get("disable_blockchain_operations", False)
+        disable_blockchain_operations = self.options.get(
+            "disable_blockchain_operations", False)
         if disable_blockchain_operations is False:
             grpc_channel = self.grpc_channel
         stub_instance = service_stub(grpc_channel)
         return stub_instance
 
-
     def _get_grpc_channel(self):
         endpoint = self.options.get("endpoint", None)
         if endpoint is None:
-            endpoint = self.servcie_metadata.get_all_endpoints_for_group(self.group["group_name"])[0]
+            endpoint = self.servcie_metadata.get_all_endpoints_for_group(
+                self.group["group_name"])[0]
         endpoint_object = urlparse(endpoint)
         if endpoint_object.port is not None:
-            channel_endpoint = endpoint_object.hostname + ":" + str(endpoint_object.port)
+            channel_endpoint = endpoint_object.hostname + \
+                ":" + str(endpoint_object.port)
         else:
             channel_endpoint = endpoint_object.hostname
 
@@ -65,19 +65,21 @@ class ServiceClient:
         elif endpoint_object.scheme == "https":
             return grpc.secure_channel(channel_endpoint, grpc.ssl_channel_credentials())
         else:
-            raise ValueError('Unsupported scheme in service metadata ("{}")'.format(endpoint_object.scheme))
-
-
+            raise ValueError('Unsupported scheme in service metadata ("{}")'.format(
+                endpoint_object.scheme))
 
     def _get_service_call_metadata(self):
         channel = self.payment_channel_management_strategy.select_channel(self)
         # change required for pricing strategy right now picking first one
-        amount = channel.state["last_signed_amount"] + int(self.group["pricing"][0]["price_in_cogs"])
+        amount = channel.state["last_signed_amount"] + \
+            int(self.group["pricing"][0]["price_in_cogs"])
         message = web3.Web3.soliditySha3(
             ["address", "uint256", "uint256", "uint256"],
-            [self.sdk.mpe_contract.contract.address,    channel.channel_id, channel.state["nonce"], amount]
+            [self.sdk.mpe_contract.contract.address,
+                channel.channel_id, channel.state["nonce"], amount]
         )
-        signature = bytes(self.sdk.web3.eth.account.signHash(defunct_hash_message(message), self.sdk.account.signer_private_key).signature)
+        signature = bytes(self.sdk.web3.eth.account.signHash(
+            defunct_hash_message(message), self.sdk.account.signer_private_key).signature)
         metadata = [
             ("snet-payment-type", "escrow"),
             ("snet-payment-channel-id", str(channel.channel_id)),
@@ -87,9 +89,8 @@ class ServiceClient:
         ]
         return metadata
 
-
     def _intercept_call(self, client_call_details, request_iterator, request_streaming,
-                       response_streaming):
+                        response_streaming):
         metadata = []
         if client_call_details.metadata is not None:
             metadata = list(client_call_details.metadata)
@@ -99,43 +100,41 @@ class ServiceClient:
             client_call_details.credentials)
         return client_call_details, request_iterator, None
 
-
     def load_open_channels(self):
         current_block_number = self.sdk.web3.eth.getBlock("latest").number
-        new_payment_channels = self.sdk.mpe_contract.get_past_open_channels(self.sdk.account, self, self.last_read_block)
+        new_payment_channels = self.sdk.mpe_contract.get_past_open_channels(
+            self.sdk.account, self, self.last_read_block)
         self.payment_channels = self.payment_channels + new_payment_channels
         self.last_read_block = current_block_number
         return self.payment_channels
-
 
     def update_channel_states(self):
         for channel in self.payment_channels:
             channel.sync_state()
         return self.payment_channels
 
-
     def default_channel_expiration(self):
         current_block_number = self.sdk.web3.eth.getBlock("latest").number
         return current_block_number + self.expiry_threshold
 
-
     def _generate_payment_channel_state_service_client(self):
         grpc_channel = self._base_grpc_channel
         with add_to_path(str(RESOURCES_PATH.joinpath("proto"))):
-            state_service_pb2_grpc = importlib.import_module("state_service_pb2_grpc")
+            state_service_pb2_grpc = importlib.import_module(
+                "state_service_pb2_grpc")
         return state_service_pb2_grpc.PaymentChannelStateServiceStub(grpc_channel)
 
-
     def open_channel(self, amount, expiration):
-        receipt = self.sdk.mpe_contract.open_channel(self.sdk.account, self, amount, expiration)
+        receipt = self.sdk.mpe_contract.open_channel(
+            self.sdk.account, self, amount, expiration)
         return self._get_newly_opened_channel(receipt)
-
 
     def deposit_and_open_channel(self, amount, expiration):
-        receipt = self.sdk.mpe_contract.deposit_and_open_channel(self.sdk.account, self, amount, expiration)
+        receipt = self.sdk.mpe_contract.deposit_and_open_channel(
+            self.sdk.account, self, amount, expiration)
         return self._get_newly_opened_channel(receipt)
 
-
     def _get_newly_opened_channel(self, receipt):
-        open_channels = self.sdk.mpe_contract.get_past_open_channels(self.sdk.account, self, receipt["blockNumber"], receipt["blockNumber"])
+        open_channels = self.sdk.mpe_contract.get_past_open_channels(
+            self.sdk.account, self, receipt["blockNumber"], receipt["blockNumber"])
         return open_channels[0]
