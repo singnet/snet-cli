1b2d91c1b495f493f12a2d8e873d03f7d79ed6dd
Pratik <pratikap98@gmail.com>
2019-11-13 11:18:44 +0530

    added print traceback in testcases

--- packages/snet_cli/test/functional_tests/script11_update_metadata.sh
+++ packages/snet_cli/test/functional_tests/script11_update_metadata.sh
@@ -1,11 +1,11 @@
 # simple case of one group
 snet service metadata-init ./service_spec1/ ExampleService --fixed-price 0.0001 --endpoints 8.8.8.8:2020 --group-name group1
 
-snet organization metadata-init org1 testo
-snet organization add-group group1 0x52653A9091b5d5021bed06c5118D24b23620c529 5.5.6.7:8089
-snet organization add-group group2 0x52653A9091b5d5021bed06c5118D24b23620c529 1.2.1.1:8089
-snet organization create testo -y -q
-snet service publish testo tests -y -q
+snet --print-traceback organization metadata-init org1 testo
+snet --print-traceback organization add-group group1 0x52653A9091b5d5021bed06c5118D24b23620c529 5.5.6.7:8089
+snet --print-traceback organization add-group group2 0x52653A9091b5d5021bed06c5118D24b23620c529 1.2.1.1:8089
+snet --print-traceback organization create testo -y -q
+snet --print-traceback service publish testo tests -y -q
 
 # change group_id
 
@@ -19,17 +19,17 @@ snet service publish testo tests -y -q
 #snet service update-metadata testo tests --metadata-file service_metadata2.json -yq && exit 1 || echo "fail as expected"
 
 # case with several groups
-snet service metadata-init ./service_spec1/ ExampleService --group-name group0 --fixed-price 0.0001 --endpoints 8.8.8.8:2020 9.8.9.8:8080
-snet service metadata-add-group group1
-snet service metadata-add-endpoints group1 8.8.8.8:22 1.2.3.4:8080
+snet --print-traceback service metadata-init ./service_spec1/ ExampleService --group-name group0 --fixed-price 0.0001 --endpoints 8.8.8.8:2020 9.8.9.8:8080
+snet --print-traceback service metadata-add-group group1
+snet --print-traceback service metadata-add-endpoints group1 8.8.8.8:22 1.2.3.4:8080
 
-snet service metadata-add-group group2
-snet service metadata-add-endpoints group2 8.8.8.8:2 1.2.3.4:800
+snet --print-traceback service metadata-add-group group2
+snet --print-traceback service metadata-add-endpoints group2 8.8.8.8:2 1.2.3.4:800
 
 # this should fail as group0 is not in organization
-snet service update-metadata testo tests -yq && exit 1 || echo "fail as expected"
-snet service metadata-remove-group group0
-snet service update-metadata testo tests -yq
+snet --print-traceback service update-metadata testo tests -yq && exit 1 || echo "fail as expected"
+snet --print-traceback service metadata-remove-group group0
+snet --print-traceback service update-metadata testo tests -yq
 
 ## change group_id   wil now be grenerated by org level
 #cat service_metadata.json | jq '.groups[1].group_id = "B5r64fQiiB5kvkWZDo7lXmo4i8y0chUvob5/CmfqoP4="' >service_metadata2.json
@@ -46,7 +46,7 @@ snet service update-metadata testo tests -yq
 
 #add assets with single value
 
-snet service metadata-init ./service_spec1/ ExampleService --metadata-file=service_asset_metadata.json
+snet --print-traceback service metadata-init ./service_spec1/ ExampleService --metadata-file=service_asset_metadata.json
 #cp service_metadata.json service_asset_metadata.json
 snet --print-traceback service metadata-add-assets ./service_spec1/test hero_image --metadata-file=service_asset_metadata.json
 snet --print-traceback service metadata-add-assets ./service_spec1/test terms_of_use --metadata-file=service_asset_metadata.json
