5d8e7061be3119522b482c4856cf0d4cc7030b85
pratik-vii <pratikap98@gmail.com>
2019-12-09 17:30:08 +0530

    removed organization only test case condition

--- packages/snet_cli/test/utils/run_all_functional.sh
+++ packages/snet_cli/test/utils/run_all_functional.sh
@@ -1,4 +1,4 @@
-for f in packages/snet_cli/test/functional_tests/script6*.sh
+for f in packages/snet_cli/test/functional_tests/*.sh
 do
    bash -ex packages/snet_cli/test/utils/reset_environment.sh --i-no-what-i-am-doing
    bash -ex -c "cd packages/snet_cli/test/functional_tests; bash -ex `basename $f`"
