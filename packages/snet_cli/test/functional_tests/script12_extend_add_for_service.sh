e38374dc87bd19759632402300c2e7c14f52279a
Rajeev <scraptorajeev@gmail.com>
2021-05-21 15:07:20 +0530

    Update script12_extend_add_for_service.sh

--- packages/snet_cli/test/functional_tests/script12_extend_add_for_service.sh
+++ packages/snet_cli/test/functional_tests/script12_extend_add_for_service.sh
@@ -8,6 +8,7 @@ snet service metadata-init ./service_spec1/ ExampleService --fixed-price 0.0001 
 snet organization create testo -y -q
 snet service publish testo tests -y -q
 
+snet account mint --amount 10000000000000 --address 0x592E3C0f3B038A0D673F19a18a773F993d4b2610 --yes
 snet account deposit 100000000 -yq
 
 snet channel open-init testo group1 123.123 1 -yq
