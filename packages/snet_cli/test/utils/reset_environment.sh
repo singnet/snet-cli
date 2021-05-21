b119b6b59f1c3597d36289018d80f2239728e331
Rajeev <scraptorajeev@gmail.com>
2021-05-22 00:57:42 +0530

    Update reset_environment.sh

--- packages/snet_cli/test/utils/reset_environment.sh
+++ packages/snet_cli/test/utils/reset_environment.sh
@@ -54,5 +54,5 @@ snet set current_multipartyescrow_at 0x5c7a4290f6f8ff64c69eeffdfafc8644a4ec3a4e
 # (snet will automatically swith to this new identity)
 snet identity create snet-user rpc --network local
 # Mint token
-python3 $cwd"/packages/snet_cli/test/functional_tests/mint/mint.py"
+python $cwd"/packages/snet_cli/test/functional_tests/mint/mint.py"
 snet account balance
