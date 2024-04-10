e4ac934f4aaa34a4c828082a9e960fa4a567654c
Pavel Yagunov <pyagunov@naint.ru>
2024-04-10 22:50:55 +0300

    Added snet.contracts==0.0.10 to common_dependencies.py

--- packages/snet_cli/common_dependencies.py
+++ packages/snet_cli/common_dependencies.py
@@ -21,7 +21,8 @@ common_dependencies = [
     'argcomplete==3.1.2',
     'grpcio-health-checking==1.59.0',
     'jsonschema==4.0.0',
-    'eth-account==0.9.0'
+    'eth-account==0.9.0',
+    'snet.contracts==0.0.10'
 ]
 
 
