b7adc6bbd66a5537c0e1ba4427209abf102dcbca
Deralden <121487413+Deralden@users.noreply.github.com>
2024-05-24 16:44:56 +0300

    Update run_all_functional.sh

--- snet/cli/test/utils/run_all_functional.sh
+++ snet/cli/test/utils/run_all_functional.sh
@@ -1,5 +1,5 @@
-for f in packages/snet_cli/test/functional_tests/*.sh
+for f in snet/cli/test/functional_tests/*.sh
 do
-   bash -ex packages/snet_cli/test/utils/reset_environment.sh --i-no-what-i-am-doing
-   bash -ex -c "cd packages/snet_cli/test/functional_tests; bash -ex `basename $f`"
+   bash -ex ./snet/cli/test/utils/reset_environment.sh --i-no-what-i-am-doing
+   bash -ex -c "cd snet/cli/test/functional_tests; bash -ex `basename $f`"
 done
