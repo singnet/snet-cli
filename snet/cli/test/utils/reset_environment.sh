5dcaddff97b750ed8aa54576d83e68081b8e5779
Deralden <121487413+Deralden@users.noreply.github.com>
2024-06-14 17:23:39 +0300

    Update reset_environment.sh

--- snet/cli/test/utils/reset_environment.sh
+++ snet/cli/test/utils/reset_environment.sh
@@ -30,29 +30,30 @@ killall node || echo "supress an error"
 # nohup ./node_modules/.bin/ganache-cli --mnemonic 'gauge enact biology destroy normal tunnel slight slide wide sauce ladder produce' --networkId 829257324 >/dev/null &
 # ./node_modules/.bin/truffle migrate --network local
 
-# # III. remove old snet-cli configuration
-# rm -rf ~/.snet
+# III. remove old snet-cli configuration
+rm -rf ~/.snet
 
-# # IV. Configure SNET-CLI.
+# IV. Configure SNET-CLI.
 
-# # set correct ipfs endpoint
-# # (the new new configuration file with default values will be created automatically)
+# set correct ipfs endpoint
+# (the new new configuration file with default values will be created automatically)
 # snet set default_ipfs_endpoint http://localhost:5002
 
-# # Add local network and switch to it
+# Add local network and switch to it
 # snet network create local http://localhost:8545
 
-# # swith to local network
+# swith to local network
 # snet network local
 
-# # Configure contract addresses for local network (it will not be necessary for goerli or mainnet! )
+# Configure contract addresses for local network (it will not be necessary for goerli or mainnet! )
 # snet set current_singularitynettoken_at 0x6e5f20669177f5bdf3703ec5ea9c4d4fe3aabd14
 # snet set current_registry_at 0x4e74fefa82e83e0964f0d9f53c68e03f7298a8b2
 # snet set current_multipartyescrow_at 0x5c7a4290f6f8ff64c69eeffdfafc8644a4ec3a4e
 
-# # Create First identity (snet-user = first ganache).
-# # (snet will automatically swith to this new identity)
+# Create First identity (snet-user = first ganache).
+# (snet will automatically swith to this new identity)
 # snet identity create snet-user rpc --network local
+snet identity create --private-key "a7638fd785fdb5cf13df0a1d7b5584cc20d4e8526403f0df105eedf23728f538" test key --network sepolia
 snet session
 export PYTHONPATH=$cwd
 # python3 $cwd"./snet/cli/test/functional_tests/mint/mint.py"
