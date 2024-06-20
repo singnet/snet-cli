c36c6e98878220a305b2914ba9756732789e10c3
kiruxaspb <dvoreckovkirill@gmail.com>
2024-06-20 17:46:30 +0300

    fix test script

--- snet/cli/test/utils/run_all_functional.sh
+++ snet/cli/test/utils/run_all_functional.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-for f in snet/cli/test/functional_tests/*.sh
-do
-   bash -ex ./snet/cli/test/utils/reset_environment.sh --i-no-what-i-am-doing
-   bash -ex -c "cd snet/cli/test/functional_tests; bash -ex `basename $f`"
-=======
 for f in snet/cli/test/functional_tests/script?_*
 do
    bash -ex ./snet/cli/test/utils/reset_environment.sh --i-no-what-i-am-doing
@@ -14,5 +8,4 @@ for f in snet/cli/test/functional_tests/script??_*
 do
    bash -ex ./snet/cli/test/utils/reset_environment.sh --i-no-what-i-am-doing
    bash -ex -c "cd snet/cli/test/functional_tests; bash -ex `basename $f`"
->>>>>>> master
 done
