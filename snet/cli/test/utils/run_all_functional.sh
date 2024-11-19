7c43c58c74a870f6767a4d2eedcdd4a1795cd510
Arondondon <andreypravdin52@gmail.com>
2024-11-19 18:37:58 +0300

    Test functional tests

--- snet/cli/test/utils/run_all_functional.sh
+++ snet/cli/test/utils/run_all_functional.sh
@@ -1,11 +1,11 @@
 for f in snet/cli/test/functional_tests/script?_*
 do
-#   bash -ex ./snet/cli/test/utils/reset_environment.sh --i-no-what-i-am-doing
+   bash -ex ./snet/cli/test/utils/reset_environment.sh --i-no-what-i-am-doing
    bash -ex -c "cd snet/cli/test/functional_tests; bash -ex `basename $f`"
 done
 
 for f in snet/cli/test/functional_tests/script??_*
 do
-#   bash -ex ./snet/cli/test/utils/reset_environment.sh --i-no-what-i-am-doing
+   bash -ex ./snet/cli/test/utils/reset_environment.sh --i-no-what-i-am-doing
    bash -ex -c "cd snet/cli/test/functional_tests; bash -ex `basename $f`"
 done
